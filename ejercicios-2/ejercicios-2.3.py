82ce8d7cc905bb29a6fdad0712c78c804f7e591b
GermaNieva <germannieva123@gmail.com>
2025-09-01 12:42:15 -0300

    actualizacion 1/9

--- ejercicios-2/ejercicios-2.3.py
+++ ejercicios-2/ejercicios-2.3.py
@@ -1,1 +1,15 @@
 #sucesion de fibonacci 
+#la succesion de fibonacci lo que hace es sumar el primer y segundo numero y al resultado sumarle el anterior ej: a+b= c , c+b = d 
+def fibonacci(num):
+    a,b=0,1  # desempaquetado
+    fibonacci_lista=[0]
+    for i in range(num):
+        if b > num: return fibonacci_lista
+        else:
+            fibonacci_lista.append(b)
+            a,b=b,a+b
+            
+resultado = fibonacci(33)
+print(resultado)
+
+
