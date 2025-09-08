14b2f1815e5681bf1c65d8c16e0739cbf6a3beea
GermaNieva <germannieva123@gmail.com>
2025-09-08 11:50:41 -0300

    actualizacion

---
+++ practica/ejercicio_practico.py
@@ -0,0 +1,45 @@
+
+monto=int(input("ingrese el monto en dolares"))
+
+canti_100= monto // 100
+monto = monto % 100
+print("canidad de billetes de 100:",canti_100)
+
+
+cantidad_50 = monto // 50 
+monto = monto%50
+
+print("canidad de billetes de 50:",cantidad_50)
+
+cantidad_20 = monto // 20
+monto = monto%20
+
+print("canidad de billetes de 20:",cantidad_20)
+
+cantidad_10 = monto // 10
+monto = monto%10
+
+print("canidad de billetes de 10:",cantidad_10)
+
+cantidad_5 = monto // 5
+monto = monto%5
+
+print("canidad de billetes de 5:",cantidad_5)
+
+cantidad_2 = monto // 2
+monto = monto%2
+
+print("canidad de billetes de 2:",cantidad_2)
+
+
+
+
+
+
+
+
+
+
+
+
+1
